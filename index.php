910d3b8e925d095f08b3237b1ed12a1587efdedb
Dennysondbs <dennyson.dbs@gmail.com>
2021-04-26 16:44:04 -0400

    atualização do arquivo index

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 
-echo "Hello word";
+//echo "Hello word";
+
+$frutas = array();
+
+array_push($frutas, array(
+	'nome'=>'Dennyson',
+	'idade'=>33
+
+));
+
+array_push($frutas, array(
+	'nome'=>'Tatiana',
+	'idade'=>38
+
+));
+
+print_r($frutas);
 
 ?>
